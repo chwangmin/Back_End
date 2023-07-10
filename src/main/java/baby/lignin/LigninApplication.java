866de0efc9e45410e230d37c31e493a39e0cab7d
wnstn819 <wnstn819@naver.com>
2023-07-10 14:53:51 +0900

    initial commit

---
+++ src/main/java/baby/lignin/LigninApplication.java
@@ -0,0 +1,13 @@
+package baby.lignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigninApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baby/lignin/LigninApplicationTests.java
@@ -0,0 +1,13 @@
+package baby.lignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
