41dd560a6dbeddb4aedb9197c1b06945e2b16208
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-12 01:56:00 +0900

    Feat : Swagger Config 설정 #3

---
+++ src/main/java/baby/lignin/config/util/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package baby.lignin.config.util;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Lignin API Specification")
+                .description("크래프톤 정글 팀.막내온탑 '리그닌' 프로젝트 API 명세입니다.")
+                .version("1.0.0");
+    }
+}
