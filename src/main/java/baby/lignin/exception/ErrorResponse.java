d8f773aaf8f6ece31886f6d51039f2e2dec1550f
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-15 17:30:11 +0900

    Feat : ErrorResponse 객체 추가 #3
    - Error 상태와 메시지를 함께 관리한다.

---
+++ src/main/java/baby/lignin/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package baby.lignin.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+    }
+}
