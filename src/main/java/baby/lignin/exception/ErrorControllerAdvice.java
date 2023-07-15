f997e87d45278be614440394d7e1b7ff424e407d
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-15 17:31:27 +0900

    Feat : exception 발생 시 전역으로 처리할 exception handler를 작성한다. #3

---
+++ src/main/java/baby/lignin/exception/ErrorControllerAdvice.java
@@ -0,0 +1,18 @@
+package baby.lignin.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse response = new ErrorResponse(ErrorCode.TEST_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Exception 추가
+}
