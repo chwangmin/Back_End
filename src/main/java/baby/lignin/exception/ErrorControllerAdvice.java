a573f9b7b8300967146a57b5eaa6fc8545b7ce23
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-16 17:51:54 +0900

    Chores : #5

--- src/main/java/baby/lignin/exception/ErrorControllerAdvice.java
+++ src/main/java/baby/lignin/exception/ErrorControllerAdvice.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorControllerAdvice {
 
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        ErrorResponse response = new ErrorResponse(ErrorCode.TEST_ERROR);
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    @ExceptionHandler(Exception.class)
+//    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+//        ErrorResponse response = new ErrorResponse(ErrorCode.TEST_ERROR);
+//        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
     // Exception 추가
 }
