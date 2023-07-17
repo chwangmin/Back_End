774b7c53a79157dc1fc915a6cf9528cc4148ff80
chwangmin <ckm7907cb@gmail.com>
2023-07-17 17:14:16 +0900

    Refactor : ci-cd 에 필요한 설정(yml, jar파일, test 무시) #7

--- src/test/java/baby/lignin/LigninApplicationTests.java
+++ src/test/java/baby/lignin/LigninApplicationTests.java
@@ -3,7 +3,7 @@ package baby.lignin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LigninApplicationTests {
 
 	@Test
