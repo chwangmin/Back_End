c6ef34e8c08821089136afbaeed0f17e17144b76
chwangmin <ckm7907cb@gmail.com>
2023-07-21 00:57:51 +0900

    Feat : 무중단 배포 blue-green test3 #19

--- script/deploy.sh
+++ script/deploy.sh
@@ -28,7 +28,7 @@ if [ -z $IS_GREEN  ];then # blue라면 (IS_GREEN이 비어 있다면)
   echo "3. green health check..."
   sleep 3
 
-  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+  REQUEST=$(curl http://ec2-43-201-1-107.ap-northeast-2.compute.amazonaws.com:8080) # green으로 request
     if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
             echo "health check success"
             break ;
@@ -53,7 +53,7 @@ else # Green라면
   while [ 1 = 1 ]; do
     echo "3. blue health check..."
     sleep 3
-    REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
+    REQUEST=$(curl http://ec2-43-201-1-107.ap-northeast-2.compute.amazonaws.com:8081) # blue로 request
 
     if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
       echo "health check success"
